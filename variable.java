42a96ded98fcd2796cc5229ad2a688f2165fa46c
BEYE ALIOUNE <alioune.beye@gmail.com>
2023-02-27 15:02:16 +0100

    push

---
+++ variable.java
@@ -0,0 +1,16 @@
+class film {
+         public static void main(String[] args){
+        String film = "Indiana Jones and the Last Crusade";
+        boolean haveYouSee = false; 
+        int annee = 1989;
+        float note = 8.2f;
+        
+              System.out.println(film);
+              System.out.println(haveYouSee);
+              System.out.println(annee);
+              System.out.println(note);
+     }
+
+
+}
+
